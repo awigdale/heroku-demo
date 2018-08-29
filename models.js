0a45836e25669d868b8463e999034f8ec530570b
abby wigdale <abby.wigdale@gmail.com>
2018-08-29 11:20:38 -0500

    fetch from db

--- models.js
+++ models.js
@@ -1,10 +1,11 @@
-const Sequelize = require('sequelize')
-const dbUrl = process.env.DATABASE_URL || 'postgres://localhost:5432/heroku-demo'
-const db = new Sequelize(dbUrl, { logging: false })
+const Sequelize = require('sequelize');
+const dbUrl =
+  process.env.DATABASE_URL || 'postgres://localhost:5432/heroku-demo';
+const db = new Sequelize(dbUrl, { logging: false });
 
 const Instructor = db.define('instructor', {
   name: Sequelize.STRING,
-  profilePic: Sequelize.STRING
-})
+  profilePic: Sequelize.STRING,
+});
 
-module.exports = { db, Instructor }
+module.exports = { db, Instructor };
